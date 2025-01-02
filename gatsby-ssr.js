991eda59b682fede75a833f41b6851bef2bc0ec2
Million D <million.dejene@gado.africa>
2025-01-02 14:35:26 +0300

    small change

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -6,7 +6,7 @@ export const onRenderBody = ({ setHeadComponents }) => {
     <link
       key="jersey"
       rel="preload"
-      href="/fonts/Jersey10Charted-Regular.ttf"
+      href="/fonts/PoetsenOne-Regular.ttf"
       as="font"
       type="font/ttf"
       crossOrigin="anonymous"
